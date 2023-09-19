a780266d32c1296a67b9f21f82ccb96dcfa6e7dd
Robert Berg <raberg1@gmail.com>
2023-09-19 11:08:48 -0500

    finished asserts 1-3

--- a2.py
+++ a2.py
@@ -21,14 +21,15 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     # keep checking as long as we haven't hit the end of either pattern or source while
     # pind is still a valid index OR sind is still a valid index (valid index means that
     # the index is != to the length of the list)
-    while "FILL IN CONDITION HERE":
+    while pind < len(pattern) or sind < len(source):
         # your job is to fill out the body of this loop
 
         # you should delete the following line
-        return ["Not done yet :)"]
+        
 
         # 1) if we reached the end of the pattern but not source
-
+        if pind == len(pattern) and sind < len(source):
+            return None
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
@@ -40,18 +41,26 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
 
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
-
+        elif pattern[pind] == source[sind]:
+            pind += 1
+            sind += 1
         # 6) else : this will happen if none of the other conditions are met it
-        # indicates the current thing it pattern doesn't match the current thing in
+        # indicates the current thing in pattern doesn't match the current thing in
         # source
+        else:
+            return None
 
     return result
 
 
 if __name__ == "__main__":
+    print(match(["x", "y", "z"], ["x", "y", "z"]))
     assert match(["x", "y", "z"], ["x", "y", "z"]) == [], "test 1 failed"
+    print(match(["x", "z", "z"], ["x", "y", "z"]))
     assert match(["x", "z", "z"], ["x", "y", "z"]) == None, "test 2 failed"
+    print(match(["x", "y"], ["x", "y", "z"]))
     assert match(["x", "y"], ["x", "y", "z"]) == None, "test 3 failed"
+
     assert match(["x", "y", "z", "z"], ["x", "y", "z"]) == None, "test 4 failed"
     assert match(["x", "_", "z"], ["x", "y", "z"]) == ["y"], "test 5 failed"
     assert match(["x", "_", "_"], ["x", "y", "z"]) == ["y", "z"], "test 6 failed"
